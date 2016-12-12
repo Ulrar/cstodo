8a43ace96eddb3cf5eaeda1d82939950433417a7
Kevin Lemonnier <kevin.lemonnier@cognix-systems.com>
2016-12-12 15:24:52 +0100

    Added a password change form

--- Foundation.hs
+++ Foundation.hs
@@ -146,11 +146,11 @@ instance Yesod App where
 
     -- Routes not requiring authentication.
     isAuthorized (AuthR _) _ = return Authorized
-    isAuthorized HomeR _ = return Authorized
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
 
+    isAuthorized HomeR _ = isLoggedIn
     isAuthorized ListsR _ = isLoggedIn
     isAuthorized (ListsForUserR _) _ = isLoggedIn
     isAuthorized (ListR _)  _ = isLoggedIn
--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,11 +1,25 @@
 module Handler.Home where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
+--import Yesod.Form.Bootstrap3
+import Yesod.Auth.HashDB
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
-  setTitle "Welcome To Yesod!"
+  setTitle "CSTodo"
   $(widgetFile "homepage")
 
+postHomeR :: Handler Html
+postHomeR = do
+  (Entity uid user) <- requireAuth
+  pass1 <- runInputPost $ ireq textField "password1"
+  pass2 <- runInputPost $ ireq textField "password2"
+  if pass1 == pass2
+    then do
+      user' <- setPassword pass1 user
+      runDB $ replace uid user'
+      setMessage "Success"
+      redirect HomeR
+    else do
+      setMessage "Password didn't match"
+      redirect HomeR
